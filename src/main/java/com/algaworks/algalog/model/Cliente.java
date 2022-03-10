20b959d39b86d4f52814983191b10e7b07b77829
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-03-10 20:29:05 -0300

    jakarta-persistence

--- src/main/java/com/algaworks/algalog/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/model/Cliente.java
@@ -1,21 +1,50 @@
 package com.algaworks.algalog.model;
 
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name = "nome") //nem precisa, nome ta igual na tabela e na classe
 	private String nome;
 	private String email;
 	private String telefone;
 	
-	public Cliente() {
-		// TODO Auto-generated constructor stub
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
 	}
-
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 	
 	
 }
