7a12303271bc314203fa8f6cf0aaf775e41ee700
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-03-10 22:39:15 -0300

    jpa-cliente

--- src/main/java/com/algaworks/algalog/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/controller/ClienteController.java
@@ -2,22 +2,29 @@ package com.algaworks.algalog.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.algaworks.algalog.model.Cliente;
+import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repository.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
 	
-	@PersistenceContext
-	private EntityManager manager;
+	//@Autowired //substituido pelo lombok onde é iniciado construtor com o clienteRepository por meio do AllArgsConstructor
+	private ClienteRepository clienteRepository;
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		return manager.createQuery("from Cliente",Cliente.class).getResultList();
+		return clienteRepository.findAll();
+	}
+	
+	@GetMapping("/clientes/{nome}")
+	public List<Cliente> listar(@PathVariable String nome) {
+		return clienteRepository.findByNome(nome);
 	}
 }
