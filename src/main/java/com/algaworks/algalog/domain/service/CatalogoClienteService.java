fd3670766553cd3847d10638df3f261719e34daf
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-03-13 12:01:35 -0300

    refatorando-update-cliente

--- src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
+++ src/main/java/com/algaworks/algalog/domain/service/CatalogoClienteService.java
@@ -18,9 +18,9 @@ public class CatalogoClienteService {
 	@Transactional
 	public Cliente salvar(Cliente cliente) {
 		
-		boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail()).isPresent();
-		
-		System.out.println("em uso? " + emailEmUso);
+		boolean emailEmUso = clienteRepository.findByEmail(cliente.getEmail())
+							.stream()
+							.anyMatch(clienteExistente -> !clienteExistente.equals(cliente));
 		
 		if( emailEmUso) {
 			throw new NegocioException("Já existe um cliente cadastrado com este e-mail.");
