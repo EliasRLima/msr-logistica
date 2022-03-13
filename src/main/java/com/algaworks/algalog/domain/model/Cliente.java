bd33cba328c9269154acc516eb10ffec63a3d405
Elias Lima <38722114+EliasRLima@users.noreply.github.com>
2022-03-13 00:20:29 -0300

    validacao-cascata

--- src/main/java/com/algaworks/algalog/domain/model/Cliente.java
+++ src/main/java/com/algaworks/algalog/domain/model/Cliente.java
@@ -1,14 +1,16 @@
 package com.algaworks.algalog.domain.model;
 
-import java.util.Objects;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import javax.validation.groups.Default;
+
+import com.algaworks.algalog.domain.ValidationGroups;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,7 +22,8 @@ import lombok.Setter;
 @Entity
 public class Cliente {
 
-	
+	@NotNull(groups = ValidationGroups.ClientId.class)
+	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
